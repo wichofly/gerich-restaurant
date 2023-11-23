7767e17d9fda108702832b7d566e3b153e5fde39
Mauricio Erazo <antonierazo13@hotmail.com>
2023-11-23 17:17:58 +0100

    feat: implement code to about us. important, learning to use "div" for Flexbox.  Here there are two base Divs (Image G and about)

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import './App.css';
 
 import { Navbar } from './components';
-import { Header } from './container';
+import { AboutUs, Header } from './container';
 
 function App() {
   return (
     <div>
       <Navbar />
       <Header />
-      {/* <AboutUs /> */}
+      <AboutUs />
     </div>
   );
 }

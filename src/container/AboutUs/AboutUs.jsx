2787aa3ae2e3ca50eb0a479df9dd781d21795d62
Mauricio Erazo <antonierazo13@hotmail.com>
2023-11-23 18:31:19 +0100

    feat: add id:about

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -3,7 +3,10 @@ import { images } from '../../constants';
 import './AboutUs.css';
 
 const AboutUs = () => (
-  <div className="app__aboutus app__bg flex__center section__padding">
+  <div
+    className="app__aboutus app__bg flex__center section__padding"
+    id="about"
+  >
     <div className="app__aboutus-overlay flex__center">
       <img src={images.G} alt="G letter" />
     </div>

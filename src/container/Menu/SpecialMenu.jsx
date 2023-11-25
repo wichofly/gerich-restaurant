256246dae3eb9f33109130b69cc6f60c13f7bae5
Mauricio Erazo <antonierazo13@hotmail.com>
2023-11-25 02:14:17 +0100

    feat: add img adn the cocktails and button

--- src/container/Menu/SpecialMenu.jsx
+++ src/container/Menu/SpecialMenu.jsx
@@ -20,11 +20,23 @@ const SpecialMenu = () => (
         </div>
       </div>
 
-      <div>
-        <img src="" alt="" />
+      <div className="app__specialMenu-menu_img">
+        <img src={images.menu} alt="Drinks" />
       </div>
 
-      <div></div>
+      <div className="app__specialMenu-menu_cocktails flex__center">
+        <p className="app__specialMenu_menu_heading">Cocktails</p>
+        <div className="app__specialMenu_menu_items">
+          {data.cocktails.map((cocktail, index, id) => (
+            <p key={id}>{cocktail.title}</p>
+          ))}
+        </div>
+      </div>
+    </div>
+    <div style={{ marginTop: '15px' }}>
+      <button type="button" className="custom__button">
+        View More
+      </button>
     </div>
   </div>
 );

f925cc34de0a39ea1a46b8ff61bd8d5e885efbe8
Mauricio Erazo <antonierazo13@hotmail.com>
2023-11-25 01:40:53 +0100

    feat: add files to use in the SpecialMenu

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css';
 
 import { Navbar } from './components';
-import { AboutUs, Header } from './container';
+import { AboutUs, Header, SpecialMenu } from './container';
 
 function App() {
   return (
@@ -9,6 +9,7 @@ function App() {
       <Navbar />
       <Header />
       <AboutUs />
+      <SpecialMenu />
     </div>
   );
 }
--- src/container/Menu/SpecialMenu.jsx
+++ src/container/Menu/SpecialMenu.jsx
@@ -1,11 +1,8 @@
-import React from 'react';
+import { images, data } from '../../constants';
+import { SubHeading, MenuItem } from '../../components';
 
 import './SpecialMenu.css';
 
-const SpecialMenu = () => (
-  <div>
-    SpecialMenu
-  </div>
-);
+const SpecialMenu = () => <div>SpecialMenu</div>;
 
 export default SpecialMenu;

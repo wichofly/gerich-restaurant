59089556a45d816daa731faf5dbd9548d675f43d
Mauricio Erazo <antonierazo13@hotmail.com>
2023-11-25 03:10:33 +0100

    fix: correct classNames style: Menuitem

--- src/components/Menuitem/MenuItem.jsx
+++ src/components/Menuitem/MenuItem.jsx
@@ -3,7 +3,7 @@ import './MenuItem.css';
 const MenuItem = ({ title, price, tags }) => (
   <div className="app__menuitem">
     <div className="app__menuitem-head">
-      <div className="menuitem-name">
+      <div className="app__menuitem-name">
         <p className="p__cormorant" style={{ color: '#DCCA87' }}>
           {title}
         </p>
@@ -11,11 +11,11 @@ const MenuItem = ({ title, price, tags }) => (
 
       <div className="app__menuitem-dash"></div>
 
-      <div className="menuitem-price">
+      <div className="app__menuitem-price">
         <p className="p__cormorant">{price}</p>
       </div>
 
-      <div className="menuitem-sub">
+      <div className="app__menuitem-sub">
         <p className="p__opensans" style={{ color: '#AAA' }}>
           {tags}
         </p>

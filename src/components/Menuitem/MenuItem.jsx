b4d5741f16b397cb00c82875c15b1b25873a9ff5
Mauricio Erazo <antonierazo13@hotmail.com>
2023-11-25 03:18:17 +0100

    fix: better view from Menuitem

--- src/components/Menuitem/MenuItem.jsx
+++ src/components/Menuitem/MenuItem.jsx
@@ -9,17 +9,17 @@ const MenuItem = ({ title, price, tags }) => (
         </p>
       </div>
 
-      <div className="app__menuitem-dash"></div>
+      <div className="app__menuitem-dash" />
 
       <div className="app__menuitem-price">
         <p className="p__cormorant">{price}</p>
       </div>
+    </div>
 
-      <div className="app__menuitem-sub">
-        <p className="p__opensans" style={{ color: '#AAA' }}>
-          {tags}
-        </p>
-      </div>
+    <div className="app__menuitem-sub">
+      <p className="p__opensans" style={{ color: '#AAAAAA' }}>
+        {tags}
+      </p>
     </div>
   </div>
 );
